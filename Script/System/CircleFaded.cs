3571fe1d397b1294fe16575f554e694079cc24eb
Aueld <44721590+Aueld@users.noreply.github.com>
2022-05-03 19:32:02 +0900

    Unity C#

--- Script/System/CircleFaded.cs
+++ Script/System/CircleFaded.cs
@@ -5,21 +5,23 @@ using UnityEngine.UI;
 
 public class CircleFaded : MonoBehaviour
 {
+    //Wait 객체가 하나만 생기게 구현
     private static WaitForSeconds Wait = new WaitForSeconds(0.001f);
 
     [SerializeField]
     float speed = 0.3f;
 
+    // 정중앙이 될 타겟 오브젝트
     GameObject target;
 
+    // 쉐이더 메테리얼
     Material runtimeMaterial;
 
     void Awake()
     {
         this.runtimeMaterial = Instantiate(GetComponent<Image>().material);
         GetComponent<Image>().material = this.runtimeMaterial;
-        target = GameObject.FindWithTag("Player");
-
+        target = GameObject.FindWithTag("Player");                  // 타겟을 플레이어로 지정
 
         StartCoroutine(Faded());
     }
@@ -36,9 +38,8 @@ public class CircleFaded : MonoBehaviour
 
         //float time = 0f;
 
-
+        // 원 지름 10
         radius = 10f;
-
 
         this.runtimeMaterial.SetFloat("_Width", rect.width);
         this.runtimeMaterial.SetFloat("_Height", rect.height);
@@ -46,7 +47,7 @@ public class CircleFaded : MonoBehaviour
 
         while (radius > 0)
         {
-
+            // 타겟을 계속 따라가게
             transform.position = target.transform.position;
             //            radius = Mathf.Abs(Mathf.Sin(Time.deltaTime * this.speed) * halfWidth);
             //radius = (Mathf.Sin(Time.deltaTime * this.speed) * halfWidth);
--- Script/System/EndingTextSystem.cs
+++ Script/System/EndingTextSystem.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class EndingTextSystem: MonoBehaviour
 {
-    public Queue<string> sentences;
-    public string currentSentence;
-    public Text text;
+    public Queue<string> sentences; // 대사 큐
+    public string currentSentence;  // TMP
+    public Text text;               // text
 
     public void Ondialogue(string[] lines)
     {
+        // string 큐 초기화
         sentences = new Queue<string>();
         sentences.Clear();
+
         foreach (var line in lines)
         {
+            // 한 큐 ( 한 문장 ) 씩 출력
             sentences.Enqueue(line);
         }
         StartCoroutine(DialogueFlow());
@@ -25,18 +28,21 @@ public class EndingTextSystem: MonoBehaviour
     {
         yield return null;
 
+        // 출력 될 string 큐가 남았을 경우
         while(sentences.Count > 0)
         {
+            // 다음 내용 출력
             currentSentence = sentences.Dequeue();
             text.text = currentSentence;
 
+            // 5초간 기다림
             yield return new WaitForSeconds(5f);
         }
 
-
+        // 5초간 기다림
         yield return new WaitForSeconds(5f);
 
+        // 메인화면으로
         LoadSceneController.LoadScene("mainStartScene");
-
     }
 }
