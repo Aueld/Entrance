07fc7ccc715098fa432ad20e2dd46a7a06bb8452
Aueld <44721590+Aueld@users.noreply.github.com>
2022-05-25 19:31:29 +0900

    Unity C#

--- Script/UI/GUIMenu.cs
+++ Script/UI/GUIMenu.cs
@@ -5,28 +5,52 @@ using UnityEngine;
 public class GUIMenu : MonoBehaviour
 {
     private bool check = false;
+    private bool mapCheck = false;
 
     // 메뉴 캔버스
     public GameObject canvas;
+    public GameObject minimap;
+    public GameObject bigmap;
 
     void Start()
     {
         // 게임 시작시
         GameManager.Instance.InitGame();
         canvas.SetActive(false);
+        bigmap.SetActive(false);
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.M) && !mapCheck)
+        {
+            bigmap.SetActive(true);
+            minimap.SetActive(false);
+            mapCheck = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.M) && mapCheck)
+        {
+            bigmap.SetActive(false);
+            minimap.SetActive(true);
+            mapCheck = false;
+        }
+
         // Esc 입력시
         if (Input.GetKeyDown(KeyCode.Escape) && !check)
         {
+            minimap.SetActive(false);
             canvas.SetActive(true);
             check = true;
             GameManager.Instance.PauseGame();   // 게임 정지
             
         } else if(Input.GetKeyDown(KeyCode.Escape) && check)
         {
+            if(mapCheck)
+            {
+                bigmap.SetActive(false);
+                mapCheck = false;
+            }
+            minimap.SetActive(true);
             canvas.SetActive(false);
             check = false;
             GameManager.Instance.ContinueGame();// 게임 재개
